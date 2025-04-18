8fdde3d172b4d2fde6baed21348e0458338aa669
ugurakcora <uakcora97@gmail.com>
2025-04-18 23:06:47 +0300

    feat: Add ignore node_modules

---
+++ assets/theme.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./main.html'],
+  theme: {
+    extend: {
+      margin: {
+        30: '7.5rem',
+        6: '10rem',
+      },
+      colors: {
+        customGray: '#666666',
+      },
+
+      width: {
+        57: '16rem',
+      },
+    },
+  },
+  fontSize: {
+    custom: '12.8px',
+  },
+  fontFamily: {
+    sans: ['ITC Avant Garde Gothic Pro', 'sans-serif'],
+  },
+};
